9ca8c65f7d76a7f688f301574bf5d456741858fb
esturcke <esturcke@carbonbox.com>
2010-11-01 12:38:19 -0400

    Adds template plugin from http://github.com/thinca/vim-template/blob/master/plugin/template.vim Adds template for perl modules

---
+++ .vim/template/template.pm
@@ -0,0 +1,33 @@
+package ???;
+
+=head1 NAME
+
+???
+
+=head1 SYNOPSIS
+
+???
+
+=cut
+
+use 5.10;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 METHODS
+
+=head2 ???
+
+  ???
+
+???
+
+=cut
+
+=head1 AUTHOR
+
+Erik J. Sturcke
+
+=cut
